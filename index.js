683e3be22e4c9ce1c8a8b45351fc125118c4a7a2
Logan Hart <2loganhart@gmail.com>
2018-07-18 14:38:42 -0700

    Add helper functions markboard, playable check with bitwise OR, validateMove, and win conditions arrays

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const board = {
   9: ' ',
 };
 
+const markBoard = (loc, icon) => {
+  board[loc] = icon.toUpperCase();
+};
+
 const printBoard = () => {
   console.log(
     `
@@ -25,4 +29,25 @@ const printBoard = () => {
     `
   );
 };
-printBoard();
+
+const playable = (val) => {
+  let x;
+  if (isNaN(val)) {
+    return false;
+  }
+  x = parseFloat(val);
+  return (x | 0) === x;
+}
+
+const validateMove = (loc) => {
+  return (playable(loc) && board[loc] === ' ');
+}
+
+const winConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+[2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+const takeTurn = (player) => {
+  console.log(`It is player ${player}'s turn`);
+  prompt.start();
+  // prompt.get([''])
+};
